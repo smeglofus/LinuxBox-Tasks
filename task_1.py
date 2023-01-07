23d23e245387c358ca404679ded04bbbf2a0f490
smeglofus <smeglofus@gmail.com>
2023-01-07 09:05:17 +0100

    documentation added

--- task_1.py
+++ task_1.py
@@ -4,9 +4,10 @@ import re
 
 # check all lines on STDIN
 for line in sys.stdin:
+    # if restart in line, print whole line
     if re.search("restart", line):
         print("\nSystem started again: ", line)
+    # if server crashed on signal, print whole line with warning that system crash is detected
     if re.search("exiting on signal", line):
-        print("\nServer crashed: ", line)
         print("\nSystem crash detected: ", line)
 
--- task_2.py
+++ task_2.py
@@ -3,13 +3,17 @@ import os
 import sys
 import fnmatch
 
+#define function, which takes 2 arguments. Directory and file name
 def find_files(directory, file_name):
+# Loop through all files and directories in directory and print found files with whole path
   for root, dirs, files in os.walk(directory):
     for file in files:
       if fnmatch.fnmatch(file, file_name):
         print(os.path.join(root, file))
 
+#script parameters
 directory = sys.argv[1]
 file_name = sys.argv[2]
 
+#call function with arguments
 find_files(directory, file_name)
